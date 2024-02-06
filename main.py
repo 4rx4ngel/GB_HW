b258e455fa5a030c0e999b6a041d9f3b83ba18fd
Mikhail_Biziukov <HostProcessor@gmail.com>
2024-02-07 00:51:27 +0700

    resolved task

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+'''
+Задача 44: В ячейке ниже представлен код генерирующий DataFrame, которая состоит всего
+из 1 столбца. Ваша задача перевести его в one hot вид. Сможете ли вы это сделать без
+get_dummies?
+import random
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+data.head()
+'''
+import pandas as pd
+import random
 
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+print(data)
+
+print('________________')
+
+data['tmp'] = 1                                         # Добавляем столбец tmp
+data.set_index([data.index, 'whoAmI'], inplace=True)    # Устанавливаем индексы по столбцу whoAmI
+data = data.unstack(level=1, fill_value=0).astype(int)  # Удаляем 1 уровень, заполняем пустые  значения нулями,
+                                                        # переводим значения в int
+data.columns = data.columns.droplevel()                 # Получаем индекс 1-го столбца и записываем его в переменную
+data.columns.name = ''                                  # Переименовываем 1-ый столбец в пустую строку
+print(data)
