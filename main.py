9dbdb2b6dde4d8c7e1e8fe4dda3506e8bbdc7705
Mikhail_Biziukov <HostProcessor@gmail.com>
2024-02-07 14:43:51 +0700

    some changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-'''
+"""
 Задача 44: В ячейке ниже представлен код генерирующий DataFrame, которая состоит всего
 из 1 столбца. Ваша задача перевести его в one hot вид. Сможете ли вы это сделать без
 get_dummies?
@@ -8,7 +8,7 @@ lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI':lst})
 data.head()
-'''
+"""
 import pandas as pd
 import random
 
@@ -20,10 +20,10 @@ print(data)
 
 print('________________')
 
-data['tmp'] = 1                                         # Добавляем столбец tmp
-data.set_index([data.index, 'whoAmI'], inplace=True)    # Устанавливаем индексы по столбцу whoAmI
-data = data.unstack(level=1, fill_value=0).astype(int)  # Удаляем 1 уровень, заполняем пустые  значения нулями,
-                                                        # переводим значения в int
-data.columns = data.columns.droplevel()                 # Получаем индекс 1-го столбца и записываем его в переменную
-data.columns.name = ''                                  # Переименовываем 1-ый столбец в пустую строку
+data['tmp'] = 1                                          # Добавляем временный столбец tmp
+data.set_index([data.index, 'whoAmI'], inplace=True)     # Устанавливаем индексы по столбцу whoAmI
+data = data.unstack(level=-1, fill_value=0).astype(int)  # Удаляем 1 уровень, заполняем пустые  значения нулями,
+                                                         # переводим значения в int
+data.columns = data.columns.droplevel()                  # Получаем индекс 1-го столбца
+data.columns.name = ''                                   # Присваиваем пустое имя
 print(data)
